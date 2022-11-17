cf202afda30b521a9ff9999599ab22a3305c8031
Alex Casella <alexcasella@alexs-mbp.myfiosgateway.com>
2022-11-17 15:31:01 -0500

    clean up; added mongoose

--- packages/hardhat/server/server.js
+++ packages/hardhat/server/server.js
@@ -1,8 +1,18 @@
 // import express from "express";
 const express = require("express");
-const { mintKYCBadgeNFT } = require("./mint/mint_nft.js");
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const { mintKYCBadgeNFT } = require("../mint/mint_nft.js");
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+  res.json({ status: "ok" });
+});
 
 app.get("/api/get", async (req, res) => {
   let address = "0x943468B770449bFc6B9fF168A428fBB45BF644f5";
